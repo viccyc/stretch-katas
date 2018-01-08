d910516380ae4c4b661897af9fcf159cc298596c
Viccy <viccy.c.grace@gmail.com>
2018-01-08 18:30:22 +0000

    completed number-guesser.js exercise

--- number-guesser.js
+++ number-guesser.js
@@ -1,64 +1,90 @@
-var numberGuesser = function (input) {
+var numberGuesser = function () {
   var randomNum = 0;
   var answersGiven = [];
   var answer = 0;
-  var numTries = 1;
+  var numTries = 0;
   var currentAnswers = 0;
+  var correctNumber = 0;
+  var alreadyGuessed = 0;
+  var tooHigh = 0;
+  var tooLow = 0;
+
+  var prompt = require('prompt-sync')();
 
   var generateRandomNum = function() {
-    randomNum = Math.floor((Math.random() * 10) + 1);
-    return randomNum;
+    return Math.floor((Math.random() * 100) + 1);;
   }
 
   var readInput = function() {
-    var prompt = require('prompt-sync')();
-
-    // code below (replace this example)
-    answer = prompt('Guess a number: ');
-    console.log('You answered: ' + answer);
-
-    answer = Number(answer);
-console.log("does answer " + answer + " === randomNum " + randomNum);
-        if (answer === randomNum) {
-           printNumTries(numTries);
-        }
-    processInput(randomNum, answer);
-  //  return answer;
-  }
-
-  var processInput = function(randomNum, answer) {
-    // add the answer to an array
-    console.log("randomNum: " + randomNum);
-//    console.log("answer: " + answer);
-      if (answersGiven.length) {
-        currentAnswers = answersGiven.length -1;
-      }
-console.log("currentAnswers: " + currentAnswers);
-    for (i = 0; i <= currentAnswers; i++) {
-      if (answer === answersGiven[i]) {
-        console.log("Already Guessed!");
-    //    answersGiven.pop();
-        readInput();
-      } else {
-        numTries++;
-//        currentAnswers = answersGiven.length;
-        console.log("Not guessed before!");
-      }
-    }
-    answersGiven.push(answer);
-    console.log("answersGiven: " + answersGiven);
-//    console.log(answersGiven);
-    readInput();
+    return prompt('Guess a number: ');
   }
 
   var printNumTries = function(numTries) {
-    console.log("numTries: " + numTries);
+    console.log("You got it! You took " + numTries + " attempts!");
     return;
   }
 
-  generateRandomNum();
-  readInput();
+  var checkNumber = function(answer) {
+    if (answer == randomNum) {
+      numTries++;
+      correctNumber = 1;
+      printNumTries(numTries);
+    } else if (answer > randomNum) {
+      tooHigh = 1;
+      processInput(randomNum, answer);
+    } else if (answer < randomNum) {
+      tooLow = 1;
+      processInput(randomNum, answer);
+    } else if (isNaN(answer)) {
+      console.log("Not a Number! Try Again!");
+    }
+  }
+
+
+
+  var processInput = function(randomNum, answer) {
+  //check if it's the first number of the program
+    if (answersGiven.length) {
+      currentAnswers = answersGiven.length -1;
+    }
+
+    alreadyGuessed = 0;
+  //go through each answer in the array to see if it's been guessed before
+    for (i = 0; i <= currentAnswers; i++) {
+      if (answer === answersGiven[i]) {
+        alreadyGuessed = 1;
+      } else {
+      }
+    }
+
+    answersGiven.push(answer);
+  //if it's not already been guessed, then check if it's too high or low and add it to attempts
+    if (alreadyGuessed == 0) {
+      numTries++;
+      if (tooLow == 1) {
+        console.log("Too Low!");
+        tooLow = 0;
+      } else if (tooHigh == 1) {
+        console.log("Too High!");
+        tooHigh = 0;
+      }
+    } else {
+        console.log("Already Guessed!");
+        tooHigh = 0;
+        tooLow = 0
+    }
+    return;
+  }
+
+  var randomNum = generateRandomNum();
+
+  while (correctNumber != 1) {
+    answer = readInput();
+    checkNumber(answer);
+  }
+
   return;
 }
 
-console.log(numberGuesser(0));
+numberGuesser();
+
